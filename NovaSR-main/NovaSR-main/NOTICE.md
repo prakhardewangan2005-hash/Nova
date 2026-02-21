1e99aea6a2f859e80883f62f77816ac9c5cee631
Prakhar Dewangan <prakhardewangan2005@gmail.com>
2026-02-21 15:28:25 +0530

    Add files via upload

---
+++ NovaSR-main/NovaSR-main/NOTICE.md
@@ -0,0 +1,4 @@
+NovaSR
+Copyright 2026 Yatharth Sharma
+
+This product includes software developed by Yatharth Sharma https://github.com/ysharma3501
---
+++ NovaSR-main/NovaSR-main/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NovaSR',
+    version='0.0.2',
+    packages=find_packages(),
+    author='ysharma3501',
+    description='A fast, small, and high-quality neural audio upsampling model.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ysharma3501/NovaSR',
+    license='MIT',
+    install_requires=[
+        'soxr',
+        'timm',
+        'torchaudio',
+        'torch',
+        'torchcodec',
+        'einops',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
